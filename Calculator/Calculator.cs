05d32785edbf61af483a7216b6b2e442aceaa814
Yumi Loh <yumilohxct@gmail.com>
2021-02-07 17:53:29 +0800

    The most basic calculator function without any validation

---
+++ Calculator/Calculator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Calculator
+{
+    class Calculator
+    {
+        public double Number1 { get; set; }
+        public double Number2 { get; set; }
+
+        public double Add()
+        {
+            return Number1 + Number2;
+        }
+
+        public double Subtract()
+        {
+            return Number1 - Number2;
+        }
+
+        public double Multiply()
+        {
+            return Number1 * Number2;
+        }
+        
+        public double Divide()
+        {
+            if (Number2 == 0)
+            {
+                throw new DivideByZeroException();
+            }
+            return Number1 / Number2;
+        }
+    }
+}
--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Runtime.Serialization;
 
 namespace Calculator
 {
@@ -6,7 +7,34 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Calculator Calculator = new Calculator();
+            Console.WriteLine($"Choose an operation: \n 1. Add \n 2. Subtract \n 3. Multiply \n 4. Divide ");
+            int option = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter first number:");
+            Calculator.Number1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter second number");
+            Calculator.Number2 = Convert.ToDouble(Console.ReadLine());
+
+            if (option == 1)
+            {
+                Console.WriteLine($"Result: {Calculator.Add()}");
+            }
+            else if (option == 2)
+            {
+                Console.WriteLine($"Result: {Calculator.Subtract()}");
+            }
+            else if (option == 3)
+            {
+                Console.WriteLine($"Result: {Calculator.Multiply()}");
+            }
+            else if (option == 4)
+            {
+                Console.WriteLine($"Result: {Calculator.Divide()}");
+            }
+            else
+            {
+                Console.WriteLine("Error Input");
+            }
         }
     }
 }
